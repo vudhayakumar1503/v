34c9f46811253067e449683187ea45554f14541f
vudhayakumar1503 <vudhayakumar1503@gmail.com>
2024-06-12 15:15:35 +0530

    1q.c

---
+++ 1q.c
@@ -0,0 +1,14 @@
+Family plays a very important role in our lives.
+  Without our family we will not have companions and who love and care for us.
+  We need family members to share our feelings, moments of joy and sorrow in life. 
+Having a healthy and prosperous family is a blessing. 
+  Everyone loves their family and so do I. In my family we are six members; 
+my mother, father, my grandfather and grandmother, my older sister and me. 
+  My parents love and pampered me and my sister. 
+  My grandparents also care for all of us a lot and are always by our side. 
+  My sister always helps me with my homework and assignments and since 
+I am very good at drawing and painting, I too help her with her art projects. As a family, 
+we always enjoying spending time together. 
+  My grandmother insists that we all must start the day by enjoying breakfast together. 
+  My father always plans fun weekend activities for all of us to do together. 
+  I love my family as they make me feel happy, safe and confident. 
